e948085b033ccfc799bde042d9fe4b58af81541a
B@k@5621 <bk209203@gmail.com>
2021-08-05 19:26:04 +0530

    Data Interactions has been commited on dated Aug 5 7:25PM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bootstrapingAngApp';
+  status = 'You haven\'t signed up yet';
+  name = '';
+
+  ontyping(event: Event) {
+
+    this.name = (<HTMLInputElement>event.target).value;
+  }
+  signup() {
+
+    this.status = 'Oops! We are working on it!';
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule
