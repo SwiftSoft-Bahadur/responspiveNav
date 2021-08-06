348ae1ed2306f89f8a9aade32f390e076fd4b5d2
B@k@5621 <bk209203@gmail.com>
2021-08-06 15:36:47 +0530

    Portfolio page contains Dashboard and OrderList on dated Aug 6 3:36

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HeaderComponent } from './header/header.component';
 import { SignupComponent } from './signup/signup.component';
 import { SigninComponent } from './signin/signin.component';
 import { ChildComponent } from './child/child.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -13,10 +16,13 @@ import { ChildComponent } from './child/child.component';
     HeaderComponent,
     SignupComponent,
     SigninComponent,
-    ChildComponent
+    ChildComponent,
+    PortfolioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
